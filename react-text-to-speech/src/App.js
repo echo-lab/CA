87261f0046c3eb6181711f4ad4d682d30faa200f
Daniel V <dan.vargasdiaz@gmail.com>
2023-02-12 18:15:37 -0500

    role play first version

--- react-text-to-speech/src/App.js
+++ react-text-to-speech/src/App.js
@@ -4,33 +4,115 @@ import mainPicture from "./Pictures/pinnochio.png";
 
 
 const firstPage = 
-["Narrator: One day, Geppetto made a little boy of wood", 
-"Pinnochio: When he finished, Geppetto sighed", 
-"Geppetto: I wish this wooden boy were real and could live here with me"];
+[ {Character: "Narrator", Dialogue: "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed"}, 
+{Character:"Geppetto", Dialogue: "I wish this wooden boy were real and could live here with me"}];
+
 
 
 
 function App() {
-  const [ourText, setOurText] = useState("")
   const msg = new SpeechSynthesisUtterance()
   msg.lang = 'en-US'
+  var character = "Narrator";
+  var stats = 0;
 
-  const speechHandler = (msg) => {
-    msg.text = ourText
-    window.speechSynthesis.speak(msg)
-  }
+
+
+  function pauser() {
+    return new Promise(resolve => {
+        let playbuttonclick = function () {
+  
+            // Remove the event from play button
+            // after clicking play button 
+            document.getElementById("continue")
+                .removeEventListener("click", playbuttonclick);
+            stats = 0;
+            resolve("resolved");
+        }
+  
+        // Here is the event listener for play
+        // button (instead of setTimeout) which
+        // will wait for the element to get click
+        // to get resolved until then it will be
+        // remain stucked inside Promise 
+        document.getElementById("continue")
+            .addEventListener("click", playbuttonclick)
+    })
+}
+
+  async function speech(page, role) {
+    for (let key in page) {
+      if(page[key].Character === role){
+      msg.text = page[key].Dialogue
+      window.speechSynthesis.speak(msg)
+      } else {
+        await pauser();
+      }
+    }
+  
+}
+
+    
+  
 
   return (
     <div className='App'>
       <h1>Pinnochio</h1>
-      <button onClick={() => speechHandler(msg)}>SPEAK</button>
+
+      <button onClick={
+        () => {speech(firstPage, character)}
+        
+        }>
+        
+        PLAY
+      
+      </button>
+
+      <button onClick={
+        () => {character = "Narrator"}
+        
+        }>
+        
+        Narrator
+      
+      </button>
+
+      <button onClick={
+        () => {character = "Geppetto"}
+        
+        }>
+        
+        Geppetto
+      
+      </button>
+
+      <button id="continue" onClick={
+        () => {}
+        
+        }>
+        
+        Continue
+      
+         </button>
+
+
       <div class="container">
+      <div class="table">
+      <table>
+        {firstPage.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.Character}</td>
+              <td>{val.Dialogue}</td>
+            </tr>
+          )
+        })}
+      </table>
+      </div>
         <div class="image">
       <img src={mainPicture} alt="Pinnochio" />
     </div>
-      <div class="text">
-        <h1>This is a beautiful picture.</h1>
-      </div>
+
       </div>
       </div>
   )
