e1de8fe7bcd7edf4d3b16937f90d12faa9686847
Daniel V <dan.vargasdiaz@gmail.com>
2023-02-19 22:50:48 -0500

    Update classes, and arrow functionality

--- react-text-to-speech/src/App.js
+++ react-text-to-speech/src/App.js
@@ -1,121 +1,163 @@
 import "./App.css"
 import { useState } from "react"
 import mainPicture from "./Pictures/pinnochio.png"; 
-
-
-const firstPage = 
-[ {Character: "Narrator", Dialogue: "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed"}, 
-{Character:"Geppetto", Dialogue: "I wish this wooden boy were real and could live here with me"}];
+import arrow  from "./Pictures/arrow.png"; 
+import React from "react";
 
 
 
 
-function App() {
-  const msg = new SpeechSynthesisUtterance()
-  msg.lang = 'en-US'
-  var character = "Narrator";
-  var stats = 0;
+
+/*class page {
+  constructor(img, text) {
+      this.img = img;
+      this.text = text;
+  }
+}*/
+
+/*class book extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Name : "",
+      Characters : [],
+      pages: []
+    };
+  }
+}*/
 
 
 
-  function pauser() {
-    return new Promise(resolve => {
-        let playbuttonclick = function () {
+class Reader extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      index : 0,
+      currentCARole: "Narrator"
+    };
+  }
+
+  getState(narrator){
+    console.log("role ", narrator)
+    this.setState({
+      ...this.state,
+      currentCARole: narrator
+    });
   
-            // Remove the event from play button
-            // after clicking play button 
-            document.getElementById("continue")
-                .removeEventListener("click", playbuttonclick);
-            stats = 0;
-            resolve("resolved");
-        }
-  
-        // Here is the event listener for play
-        // button (instead of setTimeout) which
-        // will wait for the element to get click
-        // to get resolved until then it will be
-        // remain stucked inside Promise 
-        document.getElementById("continue")
-            .addEventListener("click", playbuttonclick)
-    })
-}
-
-  async function speech(page, role) {
-    for (let key in page) {
-      if(page[key].Character === role){
-      msg.text = page[key].Dialogue
-      window.speechSynthesis.speak(msg)
-      } else {
-        await pauser();
-      }
-    }
-  
-}
-
+  }
     
-  
+    render(){
+     var newindex =0
 
-  return (
-    <div className='App'>
+      return(
+        <div className='App'>
       <h1>Pinnochio</h1>
-
-      <button onClick={
-        () => {speech(firstPage, character)}
-        
-        }>
-        
-        PLAY
       
-      </button>
+      <label>
+      VA         
+      <select
+      value = {this.state.currentCARole}
+      onChange= { e => this.getState(e.target.value)}
+      >
+      {Characters.map((val) => {
+        return(
+          <option value={val}>{val}</option>
+        )
+      }
+      )}
+      </select>
+      </label>
 
+      <div className="container">
+      <Table/>
+        <div className="image">
+      <img src={mainPicture} alt="Pinnochio" />
+    </div>
+
+      </div>
       <button onClick={
-        () => {character = "Narrator"}
-        
-        }>
-        
-        Narrator
-      
-      </button>
-
-      <button onClick={
-        () => {character = "Geppetto"}
-        
-        }>
-        
-        Geppetto
-      
-      </button>
-
-      <button id="continue" onClick={
-        () => {}
+        () => {
+          newindex = continueReading(firstPage, this.state.currentCARole, this.state.index )
+          this.setState({index : newindex, currentCARole: this.state.currentCARole});
+        }
         
         }>
         
         Continue
       
-         </button>
+      </button>
+      </div>
+      );
+    }
+    
+
+}
 
 
-      <div class="container">
-      <div class="table">
-      <table>
+  
+  
+
+
+
+
+
+function continueReading(page, role, index) {
+  const msg = new SpeechSynthesisUtterance()
+  msg.lang = 'en-US'
+  console.log("role ", role)
+
+  if(page[index].Character === role) {
+    msg.text = page[index].Dialogue
+    console.log("Reading ", msg.text)
+    window.speechSynthesis.speak(msg)
+    page[index].Reading = false;
+    index = index+1;
+    page[index].Reading = true;
+  } else {
+    page[index].Reading = false;
+    index = index+1;
+    page[index].Reading = true;
+  }
+  return index;
+}
+
+class Table extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      update : true
+    };
+  }
+
+      render(){
+        return (
+          <div className="table">
+          <table>
         {firstPage.map((val, key) => {
           return (
             <tr key={key}>
+              <td>{val.Reading && <img style={{ width: 20, height: 20 }} src ={arrow} />} </td>
               <td>{val.Character}</td>
               <td>{val.Dialogue}</td>
             </tr>
           )
         })}
-      </table>
-      </div>
-        <div class="image">
-      <img src={mainPicture} alt="Pinnochio" />
-    </div>
+        </table>
+        </div>
+        );
+      }
+    }
 
-      </div>
-      </div>
-  )
-}
+var firstPage = [ { Reading: true, Character: "Narrator", Dialogue: "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed"}, 
+  {Reading: false,Character:"Geppetto", Dialogue: "I wish this wooden boy were real and could live here with me"},
+  {Reading: false, Character: "Narrator", Dialogue: "Suddenly it happened! The little wooden boy came to life!. \n Geppetto shouted with joy and, with the laughter of happiness, said"},
+  {Reading: false,Character:"Geppetto", Dialogue: "Be Welcome! I’ll call you Pinocchio"}
+];
 
-export default App
+var Characters = ["Narrator", "Geppetto", "Pinocchio"];
+
+
+
+export default Reader
