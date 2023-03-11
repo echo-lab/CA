a795a74541e16fb24f61d1ea66fbfb1499e4c881
Daniel V <64104362+DanielDVD@users.noreply.github.com>
2023-03-10 21:08:21 -0500

    Update Book, missing table

--- react-text-to-speech/src/App.js
+++ react-text-to-speech/src/App.js
@@ -10,6 +10,8 @@ class Page {
     this.imgage = img;
     this.text = text;
   }
+
+
 }
 
 
@@ -17,29 +19,24 @@ class Book {
   constructor(name,pages,characters) {
     this.name = name;
     this.pages = pages;
+    this.characters = characters;
+    this.currentPage = 0;
+  }
+
+  getCharacters(){
+    return this.characters;
+  }
+
+  getNextPage(currentPage){
+    if(currentPage < this.pages.lenght){
+      return this.pages[currentPage+1];
+    } else{
+      return null;
+    }
+
   }
 }
-/* Create Data structure
 
-
-
-/*class page {
-  constructor(img, text) {
-      this.img = img;
-      this.text = text;
-  }
-}*/
-
-/*class book extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Name : "",
-      Characters : [],
-      pages: []
-    };
-  }
-}*/
 
 var firstPageContent = [ { Reading: false, Character: "Narrator", Dialogue: "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed"}, 
   {Reading: false,Character:"Geppetto", Dialogue: "I wish this wooden boy were real and could live here with me"},
@@ -47,18 +44,31 @@ var firstPageContent = [ { Reading: false, Character: "Narrator", Dialogue: "One
   {Reading: false,Character:"Geppetto", Dialogue: "Be Welcome! I’ll call you Pinocchio"}
 ];
 
-var Characters = ["Narrator", "Geppetto", "Pinocchio"];
+var secondPageContent = [ {Reading: false, Character: "Narrator", Dialogue: "Geppetto helped Pinocchio dress, gave him some books, \n kissed him on the cheek, and sent him to school to learn to read and write. \n But he warned him:"},
+{Reading: false, Character:"Geppetto", Dialogue:"As soon as school is finished, \n Pinocchio will come home" },
+{Reading: false, Character: "Pinocchio", Dialogue: "Yes! I will do that"},
+{Reading: false, Character: "Narrator", Dialogue: "Pinocchio happily walked towards the school."}
+];
+
+
+
+var Characters = ["Narrator", "Geppetto", "Pinocchio","Blue Fairy"];
 
 var firstPage = new Page(mainPicture, firstPageContent);
+var secondPage = new Page(mainPicture, secondPageContent);
 
-var Pinnochio_Book = new Book("Pinnochio", firstPage,Characters);
+var Pages = [firstPage,secondPage];
+
+var CurrentBook = new Book("Pinnochio", Pages,Characters);
 
 
 class Reader extends React.Component{
 
+
   constructor(props) {
     super(props);
     this.state = {
+      page: 0,
       index : 0,
       currentCARole: "Narrator"
     };
@@ -86,7 +96,7 @@ class Reader extends React.Component{
       value = {this.state.currentCARole}
       onChange= { e => this.getState(e.target.value)}
       >
-      {Characters.map((val) => {
+      {CurrentBook.getCharacters().map((val) => {
         return(
           <option value={val}>{val}</option>
         )
@@ -104,8 +114,15 @@ class Reader extends React.Component{
       </div>
       <button onClick={
         () => {
-          newindex = continueReading(firstPageContent, this.state.currentCARole, this.state.index )
-          this.setState({index : newindex, currentCARole: this.state.currentCARole});
+          console.log("Page: ",this.state.page);
+          console.log("Index: ",this.state.index)
+          if (CurrentBook.pages[this.state.page].text.length > this.state.index){
+          newindex = continueReading(CurrentBook.pages[this.state.page], this.state.currentCARole, this.state.index )
+
+          this.setState({...this.state,index : newindex});
+          } else {
+            this.setState( { ...this.state, page : +1, index: 0  }  )
+          }
         }
         
         }>
@@ -132,12 +149,14 @@ function continueReading(page, role, index) {
   const msg = new SpeechSynthesisUtterance()
   msg.lang = 'en-US'
   console.log("role ", role)
+  console.log(page);
+  console.log(index)
   if(index > 0) {
-    page[index-1].Reading = false;
+    page.text[index-1].Reading = false;
   }
-  page[index].Reading = true;
-  if(page[index].Character === role) {
-    msg.text = page[index].Dialogue
+  page.text[index].Reading = true;
+  if(page.text[index].Character === role) {
+    msg.text = page.text[index].Dialogue
     console.log("Reading ", msg.text)
     window.speechSynthesis.speak(msg)
   }
@@ -158,7 +177,7 @@ class Table extends React.Component{
         return (
           <div className="table">
           <table>
-        {firstPageContent.map((val, key) => {
+        {CurrentBook.pages[Reader.state.page].map((val, key) => { /*Check this*/ 
           return (
             <tr key={key}>
               <td>{val.Reading && <img style={{ width: 20, height: 20 }} src ={arrow} />} </td>
@@ -173,13 +192,6 @@ class Table extends React.Component{
       }
     }
 
-var firstPageContent = [ { Reading: false, Character: "Narrator", Dialogue: "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed"}, 
-  {Reading: false,Character:"Geppetto", Dialogue: "I wish this wooden boy were real and could live here with me"},
-  {Reading: false, Character: "Narrator", Dialogue: "Suddenly it happened! The little wooden boy came to life!. \n Geppetto shouted with joy and, with the laughter of happiness, said"},
-  {Reading: false,Character:"Geppetto", Dialogue: "Be Welcome! I’ll call you Pinocchio"}
-];
-
-var Characters = ["Narrator", "Geppetto", "Pinocchio"];
 
 
 
