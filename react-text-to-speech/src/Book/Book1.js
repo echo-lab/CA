f42d1ed146ebd6ca634e7f60ef8c9666c153a7e8
Sulakna Karunaratna <sulakna.karunaratna@arimaclanka.com>
2023-07-12 06:28:03 -0400

    book1

--- react-text-to-speech/src/Book/Book1.js
+++ react-text-to-speech/src/Book/Book1.js
@@ -5,244 +5,311 @@ export const data = [
       Name: "Pinnochio",
       Characters: [
         {Name:"Narrator", img: require("../Pictures/pinnochio.png")},
-        {Name:"Geppetto",img: require("../Pictures/pinnochio.png")},
-        {Name:"Pinocchio",img: require("../Pictures/pinnochio.png")},
-        {Name:"Blue Fairy",img: require("../Pictures/pinnochio.png")},
-        {Name:"Bad Men",img: require("../Pictures/pinnochio.png")},
-        {Name:"Fish",img: require("../Pictures/pinnochio.png")}
+        {Name:"Clara",img: require("../Pictures/pinnochio.png")},
+        {Name:"Zoe",img: require("../Pictures/pinnochio.png")},
       ],
 
       Pages: {
         PageOne: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_1.jpg"),
           text: [
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Clara",
               Dialogue:
-                "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed",
+                "Hey, what are these decorations for?",
             },
             {
               Reading: false,
-              Character: "Geppetto",
+              Character: "Zoe",
               Dialogue:
-                "I wish this wooden boy were real and could live here with me",
+                "Ummm, a surprise party for… uhhh… someone said Zoe. Come help!",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Zoe",
               Dialogue:
-                "Suddenly it happened! The little wooden boy came to life!. \n Geppetto shouted with joy and, with the laughter of happiness, said",
+                "Let’s choose a cake! squawked Zoe. This one has the birthday girl’s favorite colors. Purple, green, purple, green!",
             },
             {
               Reading: false,
-              Character: "Geppetto",
-              Dialogue: "Be Welcome! I’ll call you Pinocchio",
+              Character: "Clara",
+              Dialogue: "Those are my favorite colors, too! said Clara. And it’s my birthday",
             },
           ],
         },
 
         PageTwo: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_3.jpg"),
           text: [
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Zoe",
               Dialogue:
-                "Geppetto helped Pinocchio dress, gave him some books, \n kissed him on the cheek, and sent him to school to learn to read and write. \n But he warned him:",
+                "Oh… it’s your birthday? Zoe turned to the streamers.\n Um… look! Purple, green, purple, green! Same as the cake! Let’s add more!",
             },
             {
               Reading: false,
-              Character: "Geppetto",
+              Character: "Clara",
               Dialogue:
-                "As soon as school is finished, \n Pinocchio will come home",
+                "Clara hugged her tail. Who else could the party be for?",
             },
             {
               Reading: false,
-              Character: "Pinocchio",
-              Dialogue: "Yes! I will do that",
+              Character: "Zoe",
+              Dialogue: "A perfect pattern! Zoe said. “The birthday girl will love it!",
             },
             {
               Reading: false,
-              Character: "Narrator",
-              Dialogue: "Pinocchio happily walked towards the school.",
+              Character: "Clara",
+              Dialogue: "Clara looked down. Had Zoe really forgotten her birthday?",
             },
           ],
         },
         PageThree: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_4.jpg"),
           text: [
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Clara",
               Dialogue:
-                "On the way, Pinocchio noticed that there was a marionette show in the square. \n He joined them and danced so well that the puppet owner offered him five gold coins. \n Pinocchio was amazed and only thought how Geppetto would be happy when he gave him the coins.",
+                "Can you please tell me who this party is for?” Clara asked",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Zoe",
               Dialogue:
-                "Near the school, Pinocchio found two bad men. \n Because he was very naive, the two men persuaded Pinocchio to go with them to a hostel to eat and sleep. \n After eating, Pinocchio became sleepy and fell asleep quickly.",
+                "No time, no time!” Zoe exclaimed. “We need more hats!",
             },
             {
               Reading: false,
-              Character: "Pinocchio",
+              Character: "Zoe",
               Dialogue:
-                "I dream that I'm rich and that my father and I are living without difficulty and are very happy.",
+                "Beep, boop, beep, boop!” Zoe pointed at the hats",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Clara",
               Dialogue:
-                "When he awoke, these men persuaded Pinocchio to bury his gold coins in a place that they knew and said to him:",
+                "Maybe I should go…” Clara whispered, but Zoe didn’t hear",
             },
           ],
         },
         PageFour: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_7.jpg"),
           text: [
             {
               Reading: false,
-              Character: "Bad Men",
+              Character: "Zoe",
               Dialogue:
-                "The coins buried here will become a tree of money and never again your father, who is old and tired, you will need to work!",
+                "We need plates!” Zoe fluttered excitedly",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "Pinocchio did so and waited for the gold coins to turn into a money tree. \n He remained a long time until, tired, he fell asleep.",
+                "Zoe set the table.",
+            },
+            {
+              Reading: false,
+              Character: "Zoe",
+              Dialogue:
+                "Uh-oh,” Zoe cried. “I don’t know what’s missing!",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "The evil men appeared and took the gold coins while Pinocchio slept.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "When he woke up, Pinocchio saw that the coins had been taken away, and he cried. \n He did not want to go home for fear that Geppetto would be angry and sad with him. \n Without knowing what to do, Pinocchio began to walk until he found a lady dressed in blue, whom he asked for help.",
+                "Clara saw Zoe was upset. Zoe was still her best friend. Clara took a deep breath.",
             },
           ],
         },
         PageFive: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_8.jpg"),
           text: [
             {
               Reading: false,
-              Character: "Pinocchio",
-              Dialogue: "I have no house and no one to live with.",
+              Character: "Clara",
+              Dialogue: "I have an idea, offered Clara.",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "The blue fairy realized that Pinocchio was lying, and his nose began to grow!",
+                "Clara changed colors.",
             },
             {
               Reading: false,
-              Character: "Blue Fairy",
+              Character: "Clara",
               Dialogue:
-                "Go home to your father. Be a good boy and do not lie anymore.",
+                "How do I look?",
             },
             {
               Reading: false,
-              Character: "Pinocchio",
-              Dialogue: "I promise.",
+              Character: "Zoe",
+              Dialogue: "Great! Zoe said. Beep, boop, beep, boop!",
             },
           ],
         },
         PageSix: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_9.jpg"),
           text: [
             {
               Reading: false,
               Character: "Narrator",
-              Dialogue: "Pinocchio's nose returned to standard size.",
+              Dialogue: "Zoe sounded so silly that Clara started laughing.",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "Back at home, Pinocchio stopped at an amusement park, and his nose began to grow again. \n In the park, they told him that he could eat all the ice cream he wanted. \n What they did not tell him was that the ice cream would turn him into a donkey!",
+                "They turned to the balloons.",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Zoe",
               Dialogue:
-                "Pinocchio ate until he could no longer, and as soon as he became a donkey, he was sold to a circus.",
+                "Wait! There aren’t enough circle balloons. Beep, boop, beep, beep, see? said Zoe",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Clara",
               Dialogue:
-                "In the circus, he was forced to work hard and was so mistreated that, shortly thereafter, he could not even walk.",
+                "Well… let’s make a new pattern,” said Clara",
             },
           ],
         },
         PageSeven: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_10.jpg"),
           text: [
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "Since it no longer served to work in the circus, the owner had him thrown into the sea. \n As soon as he fell into the sea, he became a wooden boy again.",
+                "Clara and Zoe moved the balloons.",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Zoe",
               Dialogue:
-                "A whale that passed by saw Pinocchio and swallowed it, thinking it was food. \n Inside the whale, what was not the surprise of Pinocchio when finding Geppetto!",
+                "Star, star, circle! Star, star, circle! shouted Zoe. We fixed it!",
             },
             {
               Reading: false,
-              Character: "Geppetto",
+              Character: "Clara",
               Dialogue:
-                "I went looking for you and ended up in the belly of this whale.",
+                "Beep, beep, boop! Beep, beep, boop!” Clara laughed. Helping WAS fun",
             },
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Clara",
               Dialogue:
-                "Geppetto was frail and sick, and a fish that was also there said:",
+                "The birthday girl will love this, Clara said. I’m glad I could help.",
             },
           ],
         },
         PageEight: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book1/Page_12.jpg"),
           text: [
             {
               Reading: false,
-              Character: "Fish",
+              Character: "Narrator",
               Dialogue:
-                "Come up both of you behind my back, and I'll take you home!",
+                "Zoe couldn’t wait any longer",
+            },
+            {
+              Reading: false,
+              Character: "Zoe",
+              Dialogue:
+                "“I have something to show you!” she squealed.",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "They did so, and when they reached the house, Pinocchio took excellent care of Geppetto until he was okay.",
+                "Boom, boom, clash! Boom, boom, clash!",
+            },
+            {
+              Reading: false,
+              Character: "Zoe",
+              Dialogue:
+                "Happy birthday, happy birthday! sang Zoe. Happy birthday, Clara!",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue:
+                "For me?” Clara gasped",
+            },
+          ],
+        },
+        PageNine: {
+          img: require("../Pictures/book1/Page_14.jpg"),
+          text: [
+            {
+              Reading: false,
+              Character: "Zoe",
+              Dialogue:
+                "Of course! Zoe gave Clara a BIG hug. You’re my best friend!",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue:
+                "Awwww! Thanks, Zoe!” Clara beamed",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "The blue fairy appeared again and, seeing that Pinocchio had been so good to Geppetto, said:",
+                "Ding dong, ding dong.",
             },
             {
               Reading: false,
-              Character: "Blue Fairy",
+              Character: "Zoe",
               Dialogue:
-                "Since you are now a good boy, I will make you a real boy.",
+                "The cake’s here! Zoe said",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "And so it was. Geppetto had finally had the son he had longed for, and the two of them were happy forever!",
+                "They threw open the cake box.",
+            },
+          ],
+        },
+        PageTen: {
+          img: require("../Pictures/book1/Page_15.jpg"),
+          text: [
+            {
+              Reading: false,
+              Character: "Zoe",
+              Dialogue:
+                "Oh no! Zoe cried. “The colors don’t match the decorations. What should we do?",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue:
+                "Keep it! said Clara. It matches the pattern of the streamers. Beep, boop, beep, boop—see?",
+            },
+            {
+              Reading: false,
+              Character: "Zoe",
+              Dialogue:
+                "You’re right!” exclaimed Zoe, taking a huge bite",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue:
+                "Zoe! Clara laughed. Our friends aren’t here yet!",
+            },
+            {
+              Reading: false,
+              Character: "Narrator",
+              Dialogue:
+                "Mmm… said Zoe. It still tastes delicious!",
             },
           ],
         },
