db7d4288063b595dffd19e2ad2984953c9840b1f
Daniel V <64104362+DanielDVD@users.noreply.github.com>
2023-08-10 21:18:56 -0700

    Update server.js

--- react-text-to-speech/server/server.js
+++ react-text-to-speech/server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors'); // Make sure to require the cors module
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000', // Default to localhost if not specified
+    origin: 'http://talemate.cs.vt.edu:3000', // Default to localhost if not specified
     methods: 'POST',
     credentials: true
   };
