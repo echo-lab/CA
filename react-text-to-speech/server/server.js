048ef74bee7d7b18c7645287e9acdedd7f743a57
Daniel V <64104362+DanielDVD@users.noreply.github.com>
2023-08-18 12:43:58 -0700

    current version

--- react-text-to-speech/server/server.js
+++ react-text-to-speech/server/server.js
@@ -26,6 +26,7 @@ app.post('/synthesize', async (req, res) => {
             input: { text: req.body.text },
             voice: req.body.voice,
             audioConfig: { audioEncoding: 'MP3' },
+            speakingRate: 0.75,
         };
 
         const response = await fetch('https://texttospeech.googleapis.com/v1/text:synthesize?key=' + GOOGLE_API_KEY, {
