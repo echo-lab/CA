423abb2c60f80ccbb5f2d416ff1d12e7ce504c5a
Daniel V <64104362+DanielDVD@users.noreply.github.com>
2023-08-30 16:41:36 -0400

    Update Roles.js

--- react-text-to-speech/src/Book/Roles.js
+++ react-text-to-speech/src/Book/Roles.js
@@ -3,8 +3,8 @@ export const roles = [
     {Role:  "Mate 2" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-E'  },img: require("../Pictures/Mates-02.png")},
     {Role:  "Mate 3" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-I'  },img: require("../Pictures/Mates-03.png")},
     {Role:  "Mate 4" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-H'  },img: require("../Pictures/Mates-04.png")},
-    {Role:  "Mate 5" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Standard-J'  },img: require("../Pictures/Mates-05.png")},
-    {Role:  "Mate 6" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-G'  },img: require("../Pictures/Mates-06.png")},
+    {Role:  "Mate 5" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Standard-G'  },img: require("../Pictures/Mates-05.png")},
+    {Role:  "Mate 6" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-J'  },img: require("../Pictures/Mates-06.png")},
     {Role: "Parent", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/adult.png")},
     {Role: "Child", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/child.png")}
     //{Role: "None", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/pinnochio.png")}
