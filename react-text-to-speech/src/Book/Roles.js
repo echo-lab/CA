5d7bbca90d0b556cd80ee3a400bee58df7b36a8e
Daniel V <64104362+DanielDVD@users.noreply.github.com>
2023-08-31 09:50:39 -0400

    Update Roles.js

--- react-text-to-speech/src/Book/Roles.js
+++ react-text-to-speech/src/Book/Roles.js
@@ -6,6 +6,6 @@ export const roles = [
     {Role:  "Mate 5" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Standard-G'  },img: require("../Pictures/Mates-05.png")},
     {Role:  "Mate 6" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-J'  },img: require("../Pictures/Mates-06.png")},
     {Role: "Parent", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/adult.png")},
-    {Role: "Child", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/child.png")}
+    //{Role: "Child", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/child.png")}
     //{Role: "None", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/pinnochio.png")}
   ]
