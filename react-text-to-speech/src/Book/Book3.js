94a3c9c5c64ad1e0d83c3ce5ab853e15bea972e8
Daniel V <64104362+DanielDVD@users.noreply.github.com>
2024-02-06 15:14:22 -0500

    Book 3

--- react-text-to-speech/src/Book/Book3.js
+++ react-text-to-speech/src/Book/Book3.js
@@ -2,249 +2,105 @@ export const data = [
   {
     Book: {
       id: 1,
-      Name: "Pinnochio",
+      Name: "Pattern Pals: Levels in the Libraru",
       Characters: [
-        {Name:"Narrator", img: require("../Pictures/pinnochio.png")},
-        {Name:"Geppetto",img: require("../Pictures/pinnochio.png")},
-        {Name:"Pinocchio",img: require("../Pictures/pinnochio.png")},
+        {Name:"Narrator", img: require("../Pictures/narrator.png")},
+        {Name:"Clara",img: require("../Pictures/book1/Clara_avatar.jpg")},
+        {Name:"Zoe",img: require("../Pictures/book1/Zoe_avatar.jpg")},
       ],
 
       Pages: {
-        PageOne: {
-          img: require("../Pictures/pinnochio.png"),
+        Cover: {
+          img: require("../Pictures/book3/cover.png"),
+         
           text: [
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "One day, Geppetto made a little boy of wood. \n When he finished, Geppetto sighed",
-            },
+                "The Pattern Pals: Levels in the Library",
+            }
+          ],
+        },
+        PageOne: {
+          img: require("../Pictures/book3/1 Library.jpg"),
+          question: "Why do you think Clara asked Zoe to use her inside voice?",
+          text: [
             {
               Reading: false,
-              Character: "Geppetto",
+              Character: "Zoe",
               Dialogue:
-                "I wish this wooden boy were real and could live here with me",
+                "“Books!”",
             },
             {
               Reading: false,
               Character: "Narrator",
               Dialogue:
-                "Suddenly it happened! The little wooden boy came to life!. \n Geppetto shouted with joy and, with the laughter of happiness, said",
+                "squawked Zoe. It was her first library visit.",
             },
             {
               Reading: false,
-              Character: "Geppetto",
-              Dialogue: "Be Welcome! I’ll call you Pinocchio",
+              Character: "Clara",
+              Dialogue:
+                "“Shh!”",
+            },
+            {
+              Reading: false,
+              Character: "Narrator",
+              Dialogue: "Clara whispered.",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue:
+                "“Use your inside voice.”",
             },
           ],
         },
 
         PageTwo: {
-          img: require("../Pictures/pinnochio.png"),
+          img: require("../Pictures/book3/2 Library.jpg"),
+          question: "How would you arrange your books in stacks?",
           text: [
             {
               Reading: false,
-              Character: "Narrator",
+              Character: "Zoe",
               Dialogue:
-                "Geppetto helped Pinocchio dress, gave him some books, \n kissed him on the cheek, and sent him to school to learn to read and write. \n But he warned him:",
-            },
-            {
-              Reading: false,
-              Character: "Geppetto",
-              Dialogue:
-                "As soon as school is finished, \n Pinocchio will come home",
-            },
-            {
-              Reading: false,
-              Character: "Pinocchio",
-              Dialogue: "Yes! I will do that",
+                "“What can we do here?”",
             },
             {
               Reading: false,
               Character: "Narrator",
-              Dialogue: "Pinocchio happily walked towards the school.",
+              Dialogue:
+                "asked Zoe.",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue: "“We can pick out books,”",
+            },
+            {
+              Reading: false,
+              Character: "Narrator",
+              Dialogue:
+                "said Clara.",
+            },
+            {
+              Reading: false,
+              Character: "Clara",
+              Dialogue: "Each of my stacks has one more book than the last. Let’s count them—one book, one, two books one, two, three books.",
+            },
+            {
+              Reading: false,
+              Character: "Narrator",
+              Dialogue:
+                "The End.",
             },
           ],
         },
-        PageThree: {
-          img: require("../Pictures/pinnochio.png"),
-          text: [
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "On the way, Pinocchio noticed that there was a marionette show in the square. \n He joined them and danced so well that the puppet owner offered him five gold coins. \n Pinocchio was amazed and only thought how Geppetto would be happy when he gave him the coins.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "Near the school, Pinocchio found two bad men. \n Because he was very naive, the two men persuaded Pinocchio to go with them to a hostel to eat and sleep. \n After eating, Pinocchio became sleepy and fell asleep quickly.",
-            },
-            {
-              Reading: false,
-              Character: "Pinocchio",
-              Dialogue:
-                "I dream that I'm rich and that my father and I are living without difficulty and are very happy.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "When he awoke, these men persuaded Pinocchio to bury his gold coins in a place that they knew and said to him:",
-            },
-          ],
         },
-        PageFour: {
-          img: require("../Pictures/pinnochio.png"),
-          text: [
-            {
-              Reading: false,
-              Character: "Bad Men",
-              Dialogue:
-                "The coins buried here will become a tree of money and never again your father, who is old and tired, you will need to work!",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "Pinocchio did so and waited for the gold coins to turn into a money tree. \n He remained a long time until, tired, he fell asleep.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "The evil men appeared and took the gold coins while Pinocchio slept.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "When he woke up, Pinocchio saw that the coins had been taken away, and he cried. \n He did not want to go home for fear that Geppetto would be angry and sad with him. \n Without knowing what to do, Pinocchio began to walk until he found a lady dressed in blue, whom he asked for help.",
-            },
-          ],
-        },
-        PageFive: {
-          img: require("../Pictures/pinnochio.png"),
-          text: [
-            {
-              Reading: false,
-              Character: "Pinocchio",
-              Dialogue: "I have no house and no one to live with.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "The blue fairy realized that Pinocchio was lying, and his nose began to grow!",
-            },
-            {
-              Reading: false,
-              Character: "Blue Fairy",
-              Dialogue:
-                "Go home to your father. Be a good boy and do not lie anymore.",
-            },
-            {
-              Reading: false,
-              Character: "Pinocchio",
-              Dialogue: "I promise.",
-            },
-          ],
-        },
-        PageSix: {
-          img: require("../Pictures/pinnochio.png"),
-          text: [
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue: "Pinocchio's nose returned to standard size.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "Back at home, Pinocchio stopped at an amusement park, and his nose began to grow again. \n In the park, they told him that he could eat all the ice cream he wanted. \n What they did not tell him was that the ice cream would turn him into a donkey!",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "Pinocchio ate until he could no longer, and as soon as he became a donkey, he was sold to a circus.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "In the circus, he was forced to work hard and was so mistreated that, shortly thereafter, he could not even walk.",
-            },
-          ],
-        },
-        PageSeven: {
-          img: require("../Pictures/pinnochio.png"),
-          text: [
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "Since it no longer served to work in the circus, the owner had him thrown into the sea. \n As soon as he fell into the sea, he became a wooden boy again.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "A whale that passed by saw Pinocchio and swallowed it, thinking it was food. \n Inside the whale, what was not the surprise of Pinocchio when finding Geppetto!",
-            },
-            {
-              Reading: false,
-              Character: "Geppetto",
-              Dialogue:
-                "I went looking for you and ended up in the belly of this whale.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "Geppetto was frail and sick, and a fish that was also there said:",
-            },
-          ],
-        },
-        PageEight: {
-          img: require("../Pictures/pinnochio.png"),
-          text: [
-            {
-              Reading: false,
-              Character: "Fish",
-              Dialogue:
-                "Come up both of you behind my back, and I'll take you home!",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "They did so, and when they reached the house, Pinocchio took excellent care of Geppetto until he was okay.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "The blue fairy appeared again and, seeing that Pinocchio had been so good to Geppetto, said:",
-            },
-            {
-              Reading: false,
-              Character: "Blue Fairy",
-              Dialogue:
-                "Since you are now a good boy, I will make you a real boy.",
-            },
-            {
-              Reading: false,
-              Character: "Narrator",
-              Dialogue:
-                "And so it was. Geppetto had finally had the son he had longed for, and the two of them were happy forever!",
-            },
-          ],
-        },
+       
       },
     },
-  },
 ];
 
