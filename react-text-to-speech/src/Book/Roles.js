dfdb4d37315ef15b2d2dce279c42192ea716afda
Daniel V. <dan.vargasdiaz@gmail.com>
2024-06-18 10:08:04 -0400

    Update Roles.js

--- react-text-to-speech/src/Book/Roles.js
+++ react-text-to-speech/src/Book/Roles.js
@@ -1,10 +1,10 @@
 export const roles = [
     {Role:  "Green Grant", RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-I'  }, img: require("../Pictures/Mates-01.png")},
-    {Role:  "Yellow Yancey" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Journey-F'  },img: require("../Pictures/Mates-02.png")},
+    {Role:  "Yellow Yancey" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-G'  },img: require("../Pictures/Mates-02.png")},
     {Role:  "Violet Victor" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Wavenet-I'  },img: require("../Pictures/Mates-03.png")},
     {Role:  "Blue Beatrice" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-F'  },img: require("../Pictures/Mates-04.png")},
     {Role:  "Ruby Randy" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-D'  },img: require("../Pictures/Mates-06.png")},
-    {Role:  "Coral Carly" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Journey-O'  },img: require("../Pictures/Mates-05.png")},
+    {Role:  "Coral Carly" ,RoleParameter: {languageCode: 'en-US', name :'en-US-Neural2-H'  },img: require("../Pictures/Mates-05.png")},
     {Role: "Parent", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/adult.png")},
     //{Role: "Child", RoleParameter: {languageCode: ' ', name :''  },img: require("../Pictures/child.png")}
   ]
