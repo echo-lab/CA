1427b30053325a64c41cf02a0f938ab0acd7bbbe
Sang Won Lee <sangwonlee@talemate-new.cs.vt.edu>
2025-07-29 21:10:41 -0400

    allowed server to accept requests from another port

--- react-text-to-speech/server/server.js
+++ react-text-to-speech/server/server.js
@@ -9,7 +9,7 @@ const certPath = process.env.CERTPATH;
 console.log(keyPath);
 console.log(certPath);
 const corsOptions = {
-    origin: ['https://talemate.cs.vt.edu', 'https://128.173.237.12','http://localhost:3000', 'https://talemate-new.cs.vt.edu' ],
+    origin: ['https://talemate.cs.vt.edu', 'https://128.173.237.12','http://localhost:3000', 'https://talemate-new.cs.vt.edu', 'https://talemate-new.cs.vt.edu:3001'],
     methods: 'POST',
     credentials: true
   };
